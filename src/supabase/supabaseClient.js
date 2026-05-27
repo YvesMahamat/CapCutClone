fe49654bc6cdaa3f0fa4111562aba0205ba52d1a
yvesmahamat <fawzimahamat@gmail.com>
2026-05-27 20:59:48 +0200

    setup: integration de Supabase client et configuration gitignore

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,7 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
-const URL_PROJET = 'https://ivceprcnfxzxtdsxvmvr.supabase.co';
+// Remplacement direct des variables par leurs valeurs textuelles
+const SUPABASE_URL = 'https://ivceprcnfxzxtdsxvmvr.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable__lC9nlZFRY2xE4NgvMi0oA_dTWUStVn'; 
 
-export const supabase = createClient(URL_PROJET, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
